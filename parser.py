fd867b0a0e968142324d5c684e96b7c001a789e8
colasgrenier1 <nicolas.grenier2@mail.mcgill.ca>
2019-01-14 14:50:45 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -16,60 +16,60 @@ Today, 12:56 PMNicolas Grenier
 
 
 class StreamReader:
-    def __init__(self, fileobj):
-        self.fileobj fileobj
-    def readchar(self):
-        return self.fileobj.read(1)
+	def __init__(self, fileobj):
+		self.fileobj = fileobj
+	def readchar(self):
+		return self.fileobj.read(1)
 	
 class Newline:
-    def __init__(self, content):
-        self.content = content
+	def __init__(self, content):
+		self.content = content
 
 class Space:
-    def __init__(self, content):
-        self.content = content
+	def __init__(self, content):
+		self.content = content
 
 class Command:
-    def __init__(self, name):
-        self.name = name
+	def __init__(self, name):
+		self.name = name
 
 class Word:
-    def __init__(self, content):
-        self.content = content
+def __init__(self, content):
+self.content = content
 
 class Block:
-    def __init__(self, content=None):
-        if content:
-            self.content = content
-        else:
-            self.content = []
+	def __init__(self, content=None):
+		if content:
+			self.content = content
+		else:
+			self.content = []
 
 class Empty:
 	pass
 
 def parse(stream):
-    """
-    Returns a list of tokens
-    """
-    tokens = []
+	"""
+	Returns a list of tokens
+	"""
+	tokens = []
 
-    while True:
-        c = stream.readchar()
+	while True:
+		c = stream.readchar()
 
-        if c == "\r":
-            tokens.append(Newline("\r"))
-        elif c == "\n":
-            if tokens and tokens[-1] is Newline and tokens[-1].content == "\r":
-                tokens[-1].content = "\r\n"
-            else:
-                tokens.append(Newline("\n"))
-        elif c in [' ', '\t']:
-            if tokens and tokens[-1] is Space:
-                tokens[-1].content.append(c)
-            else:
-                tokens.apend(Space(c))
-        elif c == "@":
-            tokens.append(Command(""))
+		if c == "\r":
+			tokens.append(Newline("\r"))
+		elif c == "\n":
+			if tokens and tokens[-1] is Newline and tokens[-1].content == "\r":
+				tokens[-1].content = "\r\n"
+			else:
+				tokens.append(Newline("\n"))
+		elif c in [' ', '\t']:
+			if tokens and tokens[-1] is Space:
+				tokens[-1].content.append(c)
+			else:
+				tokens.apend(Space(c))
+		elif c == "@":
+			tokens.append(Command(""))
 		elif c == ";"
 			#Semicolons can be used to end commands without adding spaces afterwards
 			#We add an "Empty" object
@@ -80,13 +80,14 @@ def parse(stream):
 				tokens[-1].content.append(c)
 			else:
 				tokens.append(c)
-        elif c == "<":
+		elif c == "<":
 			#We append the contents of the block
 			#The ending ">" should be taken up too
 			tokens.append(Block(parse(stream)))
-		elif c == ">":
+		elif c == ">" or c == " ":
 			#we are supposed to be at the end of a brace here.
-			return toret
+			#We also return at the end of stream
+			return tokens
 		else:
 			#This is an ordinary character
 			if tokens and tokens[-1] is Word:
